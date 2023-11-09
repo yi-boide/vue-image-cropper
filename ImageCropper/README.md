576e825dbe9dc7cc606ae8adde70ac4157bdea92
Marvin <454846659@qq.com>
2023-11-09 19:08:43 +0800

    upd: 取消发布包中package.json中没必要的代码

---
+++ ImageCropper/README.md
@@ -0,0 +1,198 @@
+<!--
+ * @Author: Marvin 454846659@qq.com
+ * @Date: 2023-10-19 17:45:22
+ * @LastEditors: Marvin 454846659@qq.com
+ * @LastEditTime: 2023-10-23 16:16:11
+ * @FilePath: /vue-image-cropper/README.md
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by Marvin, All Rights Reserved. 
+-->
+# ImageCropper 图片裁剪
+
+image-cropper是一个基于vue3 typescript开发的图片裁剪组件，展示区域，裁剪大小可自由控制。
+
+### 安装
+
+```shell
+# via npm
+npm i image-cropper-next
+
+# via yarn
+yarn add image-cropper-next
+
+# via pnpm
+pnpm add image-cropper-next
+
+# via Bun
+bun add image-cropper-next
+```
+
+## 使用，全局导入(main.ts/main.js)
+
+```ts
+import { createApp } from 'vue'
+import ImageCropper from 'image-cropper-next'
+const app = createApp(App)
+app.use(ImageCropper)
+```
+
+## 使用，用的地方导入
+
+### 默认方式，弹窗模式，不穿入任何参数
+
+> 点击按钮后会打开弹窗，需要在内部选择图片，然后进行裁剪
+
+```html
+<template>
+  <image-cropper @cutDown="cutDown"></image-cropper>
+  <img style="width: 200px;" :src="imgUrl" :alt="imgUrl">
+</template>
+<script setup>
+import { ImageCropper } from 'image-cropper-next'
+const imgUrl = ref('')
+const imageCropperRef = ref()
+const cutDown = (data) => {
+  imgUrl.value = data.dataURL
+}
+</script>
+```
+
+### 远端图片裁剪，弹窗模式，不穿入任何参数
+
+> 自定义配置，通过ref触发open方法，需要传入图片地址，不传入则需要在内部选择图片进行裁剪
+> 远端图片需要配置cross-origin为true，cross-origin-header为"anonymous"
+
+```html
+<template>
+  <button @click="imageCropperRef.open(image)">打开裁剪框</button>
+  <image-cropper ref="imageCropperRef" :cross-origin="true" cross-origin-header="anonymous" @cutDown="cutDown">
+    <template #open></template>
+  </image-cropper>
+  <img style="width: 200px;" :src="imgUrl" :alt="imgUrl">
+</template>
+<script setup>
+import { ImageCropper } from 'image-cropper-next'
+const imgUrl = ref('')
+
+const image = new Image()
+image.name = 'test'
+image.src = 'https://node.wisdoms.xin/static/img/20230627/zb0XVS9bimage.png'
+
+const imageCropperRef = ref()
+const cutDown = (data) => {
+  imgUrl.value = data.dataURL
+}
+</script>
+```
+
+### 直接在页面中显示模式
+
+> 可通过onPrintImg方法实时获取裁剪后的图片信息
+
+```html
+<template>
+  <image-cropper :isModal="false" @cutDown="cutDown" @onPrintImg="onPrintImg">
+    <template #open></template>
+  </image-cropper>
+  <img style="width: 200px;" :src="imgUrl" :alt="imgUrl">
+</template>
+
+<script lang="ts" setup>
+import { ImageCropper } from 'image-cropper-next'
+import { ref } from 'vue';
+
+const imgUrl = ref('')
+const cutDown = (data: any) => {
+  console.log(data);
+  imgUrl.value = data.dataURL
+}
+const onPrintImg = (data: any) => {
+  console.log(data);
+  imgUrl.value = data.dataURL
+}
+</script>
+```
+
+## API
+
+### AvatarCropper Props
+| 属性名 | 描述 | 类型 | 默认值 | 是否必需 |
+|-------|-----|------|---------|---------|
+| cross-origin | 是否设置图片跨域 | Boolean | false | 否 |
+| cross-origin-header | 是否设置图片跨域 | String | '*' | 否 |
+| label | 按钮文字 | String | '选择图片' | 否 |
+| is-modal | 是否已弹窗形式展示 | Boolean | true | 否 |
+| lock-scroll | 是否在弹窗出现时锁定body | Boolean | true | 否 |
+| show-choose-btn | 是否显示选择图片按钮 | Boolean | true | 否 |
+| box-width | 裁剪窗口高度 | Number | 800 | 否 |
+| box-height | 裁剪窗口高度 | Number | 400 | 否 |
+| cut-width | 默认裁剪宽度 | Number | 200 | 否 |
+| cut-height | 默认裁剪高度 | Number | 200 | 否 |
+| rate | 按比例裁剪，例(2:1) | String | null | 否 |
+| tool | 是否显示工具栏 | Boolean | true | 否 |
+| tool-bg | 工具栏背景色 | String | '#fff' | 否 |
+| img-move | 能否拖动图片 | Boolean | true | 否 |
+| size-change | 能否调整裁剪尺寸 | Boolean | true | 否 |
+| original-graph | 是否为原图裁剪 | Boolean | false | 否 |
+| move-able | 能否调整裁剪区域位置 | Boolean | true | 否 |
+| preview-mode | 裁剪过程中是否返回裁剪结果 裁剪原图卡顿时将此项设置为false | Boolean | true | 否 |
+| watermark-text | 水印文字 | String | '' | 否 |
+| watermark-text-font | 水印文字样式 | String | '12px Sans-serif' | 否 |
+| watermark-text-color | 水印文字颜色 | String | '#fff' | 否 |
+| watermark-text-x | 水印横向位置 | Number | 0.95 | 否 |
+| watermark-text-y | 水印纵向位置 | Number | 0.95 | 否 |
+| small-to-upload | 选择的图片宽高均小于裁剪宽高度时候直接上传原图 | Boolean | false | 否 |
+| save-cut-position | 是否保存上一次裁剪位置 | Boolean | false | 否 |
+| scale-able | 是否允许缩放图片 | Boolean | true | 否 |
+| file-type | 文件类型 | String | 'png' | 否 |
+| tool-box-overflow | 是否允许裁剪框超出图片 | Boolean | true | 否 |
+| quality | 裁剪后的图片质量 | Number | 1 | 否 |
+| is-finish-close | 是否在裁剪完成后关闭弹窗 | Boolean | true | 否 |
+
+### AvatarCropper Slots
+
+| 名称    | 描述                                                        |
+| ------- | ----------------------------------------------------------- |
+| open | 弹窗模式，初始状态下显示的内容，设置后label文字按钮将不再显示（showChooseBtn为true时生效） |
+| title | 自定义title内容      |
+| ratio | 控制该（Ratio: ）内容 |
+| scaleReset | 控制该（Scale: ）内容 |
+| turnLeft | 控制该（↳）内容 |
+| turnRight | 控制该（↲）内容 |
+| reset | 控制该（↻）内容 |
+| flipHorizontal | 控制该（⇆）内容 |
+| flipVertically | 控制该（⇅）内容 |
+| choose | 左下角显示的选择文件按钮（showChooseBtn为true时生效） |
+| cancel | 取消按钮（清除画布/关闭弹窗） |
+| confirm | 确认按钮（确认裁剪） |
+
+
+### AvatarCropper Events
+
+| 名称    | 描述               | 回调参数           |
+| ------- | ------------------ | ------------------ |
+| cutDown | 点击确认后触发 | { fileName,blob,file,dataURL } |
+| onPrintImg | 实现渲染触发，每次更改裁剪内容都会触发 | { fileName,blob,file,dataURL } |
+| error  | 错误时触发       | 错误信息                 |
+| chooseImg | 选择图片后触发 | File/Image |
+| onClearAll  | 清除所有内容       | -                  |
+
+### AvatarCropper Ref
+
+| 事件名  | 说明      | 参数 |
+| ------- | --------- | ----- |
+| close  | 取消  | - |
+| open | 打开裁剪框  |  image图片,必须要有src属性（new Image()）  |
+
+### 样式变量
+
+组件提供了下列 CSS 变量，可用于自定义样式。
+
+| 名称               | 默认值 | 描述     |
+| ------------------ | ------ | -------- |
+| \--image-cropper-primary-color | `#409eff`  | 主题颜色 |
+| \--image-cropper-primary-color-hover | `#66b1ff`  | 主题颜色hover |
+| \--image-cropper-primary-color-light | `#c6e2ff`  | 主题颜色次色 |
+| \--image-cropper-warning-color | `#e6a23c`  | warning色调 |
+| \--image-cropper-warning-color-hover | `#ebb563`  | warning色调hover |
--- scripts/generate-types.cjs
+++ scripts/generate-types.cjs
@@ -2,55 +2,73 @@
  * @Author: Marvin 454846659@qq.com
  * @Date: 2023-10-20 15:27:55
  * @LastEditors: Marvin 454846659@qq.com
- * @LastEditTime: 2023-10-23 16:47:25
+ * @LastEditTime: 2023-11-09 19:08:27
  * @FilePath: /vue-image-cropper/scripts/generate-types.cjs
- * @Description: 
- * 
- * Copyright (c) 2023 by Marvin, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2023 by Marvin, All Rights Reserved.
  */
-const path = require('path');
-const fs = require('fs');
-const MarkdownIt = require('markdown-it');
-const config = require('../package.json');
+const path = require("path");
+const fs = require("fs-extra");
+const MarkdownIt = require("markdown-it");
+const config = require("../package.json");
 
-const TYPE_IDENTIFY_OPEN = 'tbody_open';
-const TYPE_IDENTIFY_CLOSE = 'tbody_close';
-const TR_TYPE_IDENTIFY_OPEN = 'tr_open';
-const TR_TYPE_IDENTIFY_CLOSE = 'tr_close';
-const sourceDir = path.resolve(__dirname, './../tsc/type/packages/ImageCropper'); // 拷贝的源文件夹
-const toDir = path.resolve(__dirname, './../dist/types'); // types
-const webTypes = path.resolve(__dirname, './../dist/smartips/web-types.json'); // smartips/web-types.json
-const indexDir = path.resolve(__dirname, './../dist/types/index.d.ts'); // types/index.d.ts
+const TYPE_IDENTIFY_OPEN = "tbody_open";
+const TYPE_IDENTIFY_CLOSE = "tbody_close";
+const TR_TYPE_IDENTIFY_OPEN = "tr_open";
+const TR_TYPE_IDENTIFY_CLOSE = "tr_close";
+const sourceDir = path.resolve(
+  __dirname,
+  "./../tsc/type/packages/ImageCropper"
+); // 拷贝的源文件夹
+const toDir = path.resolve(__dirname, "./../dist/types"); // types
+const webTypes = path.resolve(__dirname, "./../dist/smartips/web-types.json"); // smartips/web-types.json
+const indexDir = path.resolve(__dirname, "./../dist/types/index.d.ts"); // types/index.d.ts
 const appendIndexContent = `\ndeclare module 'vue' {
   export interface GlobalComponents {
       ImageCropper: typeof import("./index.vue")["default"]
   }
-}`
+}`;
 const genTypes = () => {
-  fs.cp(sourceDir, toDir, { recursive: true }, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('ts types copy success');
-    fs.appendFile(indexDir, appendIndexContent, (err) => {
+  return new Promise((resolve, reject) => {
+    fs.cp(sourceDir, toDir, { recursive: true }, (err) => {
       if (err) {
         console.error(err);
+        reject(err);
         return;
       }
-      console.log('ts types append success');
-    })
-  })
-}
+      console.log("ts types copy success");
+      fs.appendFile(indexDir, appendIndexContent, (err) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+          return;
+        }
+        console.log("ts types append success");
+        resolve();
+      });
+    });
+  });
+};
 
 const getSubSources = (sources) => {
   let sourcesMap = [];
-  const startIndex = sources.findIndex((source) => source.type === TYPE_IDENTIFY_OPEN);
-  const endIndex = sources.findIndex((source) => source.type === TYPE_IDENTIFY_CLOSE);
+  const startIndex = sources.findIndex(
+    (source) => source.type === TYPE_IDENTIFY_OPEN
+  );
+  const endIndex = sources.findIndex(
+    (source) => source.type === TYPE_IDENTIFY_CLOSE
+  );
   sources = sources.slice(startIndex, endIndex + 1);
-  while (sources.filter((source) => source.type === TR_TYPE_IDENTIFY_OPEN).length) {
-    let trStartIndex = sources.findIndex((source) => source.type === TR_TYPE_IDENTIFY_OPEN);
-    let trEndIndex = sources.findIndex((source) => source.type === TR_TYPE_IDENTIFY_CLOSE);
+  while (
+    sources.filter((source) => source.type === TR_TYPE_IDENTIFY_OPEN).length
+  ) {
+    let trStartIndex = sources.findIndex(
+      (source) => source.type === TR_TYPE_IDENTIFY_OPEN
+    );
+    let trEndIndex = sources.findIndex(
+      (source) => source.type === TR_TYPE_IDENTIFY_CLOSE
+    );
     sourcesMap.push(sources.slice(trStartIndex, trEndIndex + 1));
     sources.splice(trStartIndex, trEndIndex - trStartIndex + 1);
   }
@@ -59,58 +77,92 @@ const getSubSources = (sources) => {
 
 const genaratorWebTypes = () => {
   let attributes = [];
-  const data = fs.readFileSync(path.resolve(__dirname, '../README.md'), 'utf8');
-  const md = new MarkdownIt()
-  const sources = md.parse(data, {})
+  const data = fs.readFileSync(path.resolve(__dirname, "../README.md"), "utf8");
+  const md = new MarkdownIt();
+  const sources = md.parse(data, {});
   let sourcesMap = getSubSources(sources);
   for (let sourceMap of sourcesMap) {
-    const inlineItem = sourceMap.filter((source) => source.type === 'inline').length
-      ? sourceMap.filter((source) => source.type === 'inline')
+    const inlineItem = sourceMap.filter((source) => source.type === "inline")
+      .length
+      ? sourceMap.filter((source) => source.type === "inline")
       : [];
-    const propItem = inlineItem.length ? `${inlineItem[0]?.content?.replace(/`.*?`/g, '')}` : '';
-    const infoItem = inlineItem.length ? `${inlineItem[1]?.content}` : '';
-    const typeItem = inlineItem.length ? `${inlineItem[2]?.content?.toLowerCase()}` : '';
-    const defaultItem = inlineItem.length ? `${inlineItem[3]?.content}` : '';
+    const propItem = inlineItem.length
+      ? `${inlineItem[0]?.content?.replace(/`.*?`/g, "")}`
+      : "";
+    const infoItem = inlineItem.length ? `${inlineItem[1]?.content}` : "";
+    const typeItem = inlineItem.length
+      ? `${inlineItem[2]?.content?.toLowerCase()}`
+      : "";
+    const defaultItem = inlineItem.length ? `${inlineItem[3]?.content}` : "";
     attributes.push({
       name: propItem,
       default: defaultItem,
       description: infoItem,
       value: {
         type: typeItem,
-        kind: 'expression'
-      }
+        kind: "expression",
+      },
     });
   }
 
   return {
-    "$schema": "https://raw.githubusercontent.com/JetBrains/web-types/master/schema/web-types.json",
-    "framework": "vue",
-    "name": "ImageCropper",
-    "version": config.version,
-    "contributions": {
-      "html": {
-        "tags": [
+    $schema:
+      "https://raw.githubusercontent.com/JetBrains/web-types/master/schema/web-types.json",
+    framework: "vue",
+    name: "ImageCropper",
+    version: config.version,
+    contributions: {
+      html: {
+        tags: [
           {
-            "name": "image-cropper",
-            "slots": [],
-            "events": [],
-            "attributes": attributes.slice()
-          }
-        ]
-      }
-    }
+            name: "image-cropper",
+            slots: [],
+            events: [],
+            attributes: attributes.slice(),
+          },
+        ],
+      },
+    },
   };
-}
+};
 
 const genWebTypes = () => {
-  const typesData = genaratorWebTypes();
-  let innerText = `${JSON.stringify(typesData, null, 2)}`;
-  const distPath = path.resolve(__dirname, './../dist');
-  if (!fs.existsSync(path.join(distPath + '/smartips'))) {
-    fs.mkdirSync(path.join(distPath + '/smartips'));
-  }
-  fs.writeFileSync(webTypes, innerText);
-  console.log('create web-types.json success');
-}
-genTypes()
-genWebTypes()
+  return new Promise((resolve) => {
+    const typesData = genaratorWebTypes();
+    let innerText = `${JSON.stringify(typesData, null, 2)}`;
+    const distPath = path.resolve(__dirname, "./../dist");
+    if (!fs.existsSync(path.join(distPath + "/smartips"))) {
+      fs.mkdirSync(path.join(distPath + "/smartips"));
+    }
+    fs.writeFileSync(webTypes, innerText);
+    console.log("create web-types.json success");
+    resolve();
+  });
+};
+
+const copy = () => {
+  const targetPkgPath = path.resolve(__dirname, `../ImageCropper/package.json`);
+  const targetPkgStr = fs.readFileSync(targetPkgPath);
+  const targetPkgObj = JSON.parse(targetPkgStr);
+  targetPkgObj.version = config.version;
+  const copyAll = [
+    fs.copy(
+      path.resolve(__dirname, "../dist"),
+      path.resolve(__dirname, "../ImageCropper/dist")
+    ),
+    fs.copy(
+      path.resolve(__dirname, "../README.md"),
+      path.resolve(__dirname, "../ImageCropper/README.md")
+    ),
+    fs.outputFile(targetPkgPath, JSON.stringify(targetPkgObj, null, 2), "utf8"),
+  ];
+  Promise.all(copyAll).then(() => {
+    console.log(`复制成功`);
+  });
+};
+Promise.all([
+  genTypes(),
+  genWebTypes(),
+]).then(() => {
+  copy();
+})
